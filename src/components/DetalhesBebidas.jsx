325988e39f51d3f5b429ae83e69278bcabb83c57
Vinicius Norberto <vpereira.norberto@gmail.com>
2021-11-08 23:01:35 -0300

    COrreção Lint

--- src/components/DetalhesBebidas.jsx
+++ src/components/DetalhesBebidas.jsx
@@ -58,7 +58,6 @@ export default function DetalhesReceitas() {
               <button type="button" data-testid="share-btn">Compartilhar</button>
               <button type="button" data-testid="favorite-btn">Favoritar</button>
               <h6 data-testid="recipe-category">{ receita.strAlcoholic}</h6>
-              <ul>
               <button
                 type="button"
                 data-testid="favorite-btn"
